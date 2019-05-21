d3c6b5ca24b36f150d8fa1cd7ee8234d99f2b5ae
Matthew Grossman <matthewryangrossman@gmail.com>
2019-05-20 20:04:45 -0500

    add in checkin capabilities

--- SouthwestAPI.py
+++ SouthwestAPI.py
@@ -7,6 +7,7 @@ from typing import List
 from typing import NamedTuple
 
 import requests
+from requests import Session
 
 import utils
 
@@ -47,19 +48,20 @@ class SouthwestAPI:
 
     def __init__(self, confirmation_num: str, first_name: str, last_name: str) -> None:
 
-        self._url_args = "/{confirmation_num}?first-name={first_name}&last-name={last_name}".format(
-            confirmation_num=confirmation_num,
-            first_name=first_name,
-            last_name=last_name
-        )
-        self._view_reservation_url = self.VIEW_RESERVATION_URL + self._url_args
-        self._checkin_url = self.CHECKIN_URL + self._url_args
+        self._confirmation_num = confirmation_num
+        self._first_name = first_name
+        self._last_name = last_name
 
-    def _request(self, url: str) -> requests.Response:
-        return requests.get(url, headers={'x-api-key': self.SW_API_KEY})
+        self._url_args = f"/{confirmation_num}?first-name={first_name}&last-name={last_name}"
+        self._checkin_post_url = self.CHECKIN_URL
+        self._view_reservation_url = self.VIEW_RESERVATION_URL + self._url_args
+        self._checkin_get_url = self.CHECKIN_URL + self._url_args
+
+        self._req_session = Session()
+        self._req_session.headers['x-api-key'] = self.SW_API_KEY
 
     def get_reservation_info(self) -> List[FlightInfo]:
-        data = self._request(self._view_reservation_url).json()
+        data = self._req_session.get(url=self._view_reservation_url).json()
         flight_info_dicts = data['viewReservationViewPage']['shareDetails']['flightInfo']
         return [FlightInfo.from_flight_info_dict(fid) for fid in flight_info_dicts]
 
@@ -75,12 +77,29 @@ class SouthwestAPI:
 
     @utils.retry([IneligibleToCheckinError])
     def checkin(self) -> None:
-        resp = self._request(self._checkin_url)
+        resp = self._checkin_get()
         data = resp.json()
         if not resp.ok:
             if data.get('messageKey') == Errors.BEFORE_CHECKIN_ERROR:
                 raise IneligibleToCheckinError
             raise Exception(f"Unknown error: {data}")
+
+        check_in_token = data['checkInSessionToken']
+        resp = self._checkin_post(check_in_token=check_in_token)
+        if not resp.ok:
+            raise Exception(f"Unknown error: {resp.json()}")
+
+    def _checkin_get(self) -> requests.Response:
+        return self._req_session.get(url=self._checkin_get_url)
+
+    def _checkin_post(self, check_in_token: str) -> requests.Response:
+        data = {
+            'checkInSessionToken': check_in_token,
+            'firstName': self._first_name,
+            'lastName': self._last_name,
+            'recordLocator': self._confirmation_num,
+        }
+        return self._req_session.post(url=self._checkin_post_url, data=data)
 
 
 if __name__ == '__main__':
