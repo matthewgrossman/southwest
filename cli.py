1c70190c176a6a370f442ae9332d12839451447e
Matthew Grossman <matthewryangrossman@gmail.com>
2019-06-17 09:32:50 -0700

    integratin checkin script

--- cli.py
+++ cli.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 
 import click
-from southwest import Southwest
+from southwest_api import SouthwestAPI
 
 
 class Actions:
     SCHEDULE = 'schedule'
-    CHECK_IN = 'check-in'
+    CHECK_IN = 'checkin'
 
 
 @click.command()
-@click.option('-a', '--action', required=True, type=click.Choice([Actions.SCHEDULE, Actions.CHECK_IN]))
 @click.option('-c', '--confirmation-num', required=True, help='flight confirmation number (e.g. LFKWNT)')
 @click.option('-f', '--first-name', required=True, help='first name (e.g. John)')
 @click.option('-l', '--last-name', required=True, help='last name (e.g. Smith)')
+@click.option(
+    '-a', '--action',
+    required=True,
+    type=click.Choice([Actions.SCHEDULE, Actions.CHECK_IN]),
+    default=Actions.SCHEDULE
+)
 def cli(action: str, confirmation_num: str, first_name: str, last_name: str) -> None:
-    southwest = Southwest(confirmation_num=confirmation_num, first_name=first_name, last_name=last_name)
+    southwest_api = SouthwestAPI(confirmation_num=confirmation_num, first_name=first_name, last_name=last_name)
     if action == Actions.CHECK_IN:
-        southwest.check_in()
+        southwest_api.checkin()
     else:
-        southwest.schedule()
+        southwest_api.schedule()
 
 
 if __name__ == '__main__':
